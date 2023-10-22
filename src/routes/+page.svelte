99f97db72fad2f03a565658c26f442bbda54545b
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-10-22 16:25:45 -0400

    getting basic site skeleton setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page">
+    <!-- Section 1 -->
+    <div class="section odd">
+        <h1 class="header">Court's Custom Sites</h1>
+    </div>
+
+    <!-- Section 2 -->
+    <div class="section even">
+        <h2>Do you freelance or own a small business in Vermont?</h2>
+    </div>
+
+    <!-- Section 3 -->
+    <div class="section odd">
+        <h2>Are you tired of your</h2>
+
+        <h2>Wix</h2>
+        <h2>Weebly</h2>
+        <h2>Squarespace</h2>
+        <h2>Wordpress</h2>
+
+        <h2>website?</h2>
+    </div>
+
+    <!-- Section 4 -->
+    <div class="section even">
+        <h2>Do you crave more custom style or functionality?</h2>
+    </div>
+
+    <!-- Section 5 -->
+    <div class="section odd">
+        <h2>Let's chat!</h2>
+        <h2>fay.courtney@gmail.com</h2>
+    </div>
+</div>
+
+<style>
+    /* css reset */
+    :global(body) { 
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .page {
+        /* font */
+        color: white;
+
+        /* page layout */
+        display: flex;
+        flex-direction: column;
+    }
+    
+    .section {
+        min-height: 90vh;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .odd {
+        background-color: hotpink;
+    }
+
+    .even {
+        background-color: orange;
+    }
+    
+    /* .header {
+        background-color: orange;
+    } */
+</style>
